1283b65e511ea7d479fc4bcd128875c95c5ce578
CarloSalaris <carloa.salaris@gmail.com>
2023-06-05 21:33:52 +0200

    logo header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,16 +8,19 @@ export default {
 </script>
 
 <template>
-    <header class="bg-white p-3">
+    <header class="bg-white p-3 d-flex align-items-center">
         <a href="#">
-            LOGO
-            <img src="" alt="">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg" alt="Logo">
         </a>
         <h1>{{ message }}</h1>
     </header>
 </template>
 
 <style lang="scss" scoped>
+header a img {
+    width: 70px;
+}
+
 h1 {
     display: inline-block;
     margin: 0 10px;
