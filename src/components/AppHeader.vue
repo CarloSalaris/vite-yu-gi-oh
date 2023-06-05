7564e1aba4355137a9012eee9f1c0282a79f3bf8
CarloSalaris <carloa.salaris@gmail.com>
2023-06-05 17:24:13 +0200

    AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: 'AppHeader',
+    props: [
+        'message'
+    ]
+}
+</script>
+
+<template>
+    <header class="bg-white p-3">
+        <a href="#">
+            LOGO
+            <img src="" alt="">
+        </a>
+        <h1>{{ message }}</h1>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+    display: inline-block;
+    margin: 0 10px;
+}
+</style> 
