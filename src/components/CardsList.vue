884f59df780e3bfdf9c69e67a9f143c6fedfb18b
CarloSalaris <carloa.salaris@gmail.com>
2023-06-06 10:21:52 +0200

    Card counter

--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -19,10 +19,17 @@ export default {
 <template>
     <section class="container bg-white p-5">
 
-        <div class="row justify-content-center">
+        <div class="row justify-content-between">
+            <div class="cardCounter px-1 py-2 mx-1 text-white">Found {{ store.cardsList.length }} cards </div>
             <!-- Card esempio statica -->
             <SingleCard v-for="singleCard in store.cardsList" :key="singleCard.id" :details="singleCard" />
         </div>
 
     </section>
 </template>
+
+<style lang="scss" scoped>
+.cardCounter {
+    background-color: #212529;
+}
+</style>
--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -11,12 +11,14 @@ export default {
 
 <template>
     <!-- Card esempio statica -->
-    <div class="text-center p-0 m-2 col-lg-2 col-md-3 col-sm-5">
-        <img :src="details.card_images[0].image_url" alt="Immagine card">
-        <div class="p-2">
-            <h6 class="text-white">{{ details.name.toUpperCase() }}</h6>
-            <div>{{ details.archetype }}</div>
-        </div>
+    <div class="text-center p-0 m-1 col-lg-2 col-md-3 col-sm-5">
+        <a href="#">
+            <img :src="details.card_images[0].image_url" alt="Immagine card">
+            <div class="p-2">
+                <h6 class="text-white">{{ details.name.toUpperCase() }}</h6>
+                <div>{{ details.archetype }}</div>
+            </div>
+        </a>
     </div>
 </template>
 <style lang="scss" scoped>
