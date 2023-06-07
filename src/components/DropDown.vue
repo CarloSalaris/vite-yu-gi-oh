ba4a2c37bdaa8afd74d7c76afb3a50be55e16aef
CarloSalaris <carloa.salaris@gmail.com>
2023-06-07 10:12:23 +0200

    select style

--- src/components/DropDown.vue
+++ src/components/DropDown.vue
@@ -20,32 +20,36 @@ export default {
 
 <template>
     <!-- <div class="dropdown">
-            <button class="btn btn-secondary btn-sm dropdown-toggle my-3 bg-white text-black" type="button"
-                data-bs-toggle="dropdown" aria-expanded="false">
-                Select archetype...
-            </button>
-            <ul class="dropdown-menu">
-                <li @click="archSelectFunc(this.emptyString)" @click.prevent="$emit('archSelected')"><a class=" dropdown-item"
-                        href="#">All</a>
-                </li>
-                <li v-for="(item, idx) in store.archList" :key="idx" @click="archSelectFunc(item.archetype_name)"
-                    @click.prevent="$emit('archSelected')">
-                    <a class=" dropdown-item" href="#">{{ item.archetype_name
-                    }}</a>
-                </li>
-            </ul>
-        </div> -->
+        <button class="btn btn-secondary btn-sm dropdown-toggle my-3 bg-white text-black" type="button"
+            data-bs-toggle="dropdown" aria-expanded="false">
+            Select archetype...
+        </button>
+        <ul class="dropdown-menu">
+            <li @click="archSelectFunc(this.emptyString)" @click.prevent="$emit('archSelected')"><a class=" dropdown-item"
+                    href="#">All</a>
+            </li>
+            <li v-for="(item, idx) in store.archList" :key="idx" @click="archSelectFunc(item.archetype_name)"
+                @click.prevent="$emit('archSelected')">
+                <a class=" dropdown-item" href="#">{{ item.archetype_name
+                }}</a>
+            </li>
+        </ul>
+    </div> -->
 
-    <select name="archetypes" id="archetype-select" v-model="store.archSelect" @change="$emit('archSelected')">
+    <!-- SELECT MENU -->
+    <select name="archetypes" id="archetype-select" v-model="store.archSelect" @change="$emit('archSelected')"
+        class="btn btn-secondary btn-sm dropdown-toggle my-3 bg-white text-black">
+        <!-- Option to reset -->
         <option value="">Select all</option>
+        <!-- List of archetypes -->
         <option v-for="(item, idx) in store.archList" :key="idx" :value="item.archetype_name">{{ item.archetype_name }}
         </option>
     </select>
 </template>
 
 <style lang="scss" scoped>
-.dropdown-menu {
-    max-height: 250px;
-    overflow: scroll;
-}
+/* .dropdown-menu {
+     max-height: 250px;
+     overflow: scroll;
+ } */
 </style>
